2e83cb1721594b9fc9cbd7d464c31641bccabd67
QuanNgHa <quanngha.dev@gmail.com>
2020-03-10 23:20:17 +0800

    Add Token Auth

--- index.js
+++ index.js
@@ -1,14 +1,84 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { User } = require('./models/user');
+const { auth } = require('./middleware/auth')
+const config = require('./config/key');
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(5000);
 
-const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://QN:1234@react-blog-xiuyz.gcp.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect(config.mongoURI,
     { useNewUrlParser: true, }).then(() => console.log('DB Connected'))
     .catch(err => console.error(err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//Use AUTH as a middleware
+app.get("/api/user/auth", auth, (req, res) => {
+    res.status(200).json({
+        _id: req._id,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role
+
+    })
+})
+
+
+app.post('/api/user/register', (req, res) => {
+    const user = new User(req.body);
+
+    //Save information in Mongo DB
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true
+        });
+    });
+
+})
+
+app.post('/api/user/login', (req, res) => {
+    //Find the email 
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user)
+            return res.json({
+                loginSuccess: false,
+                message: "Auth failed, email not found"
+            });
+        //comparePassword
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.json({
+                    loginSuccess: false,
+                    message: "Wrong password"
+                })
+            }
+        })
+        //generateToken 
+        user.generateToken((err, user) => {
+            if (err) return res.status(400).send(err);
+            res.cookie("x_auth", user.token).status(200).json({
+                loginSuccess: true
+            })
+        })
+    })
+
+
+})
+
+
+app.listen(5000);
+
