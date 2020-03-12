38bb4a0fcf2d51c8fe08a3e0f9d63ef1f3bb820a
QuanNgHa <quanngha.dev@gmail.com>
2020-03-12 23:38:03 +0800

    Add concurrently for both backend and frontend

--- client/src/components/about/index.js
+++ client/src/components/about/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class index extends Component {
+export default class About extends Component {
     render() {
         return (
             <div>
-
+                This is about page
             </div>
         )
     }
